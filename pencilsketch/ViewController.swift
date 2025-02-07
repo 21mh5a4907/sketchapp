6c61cafb218f8b269e29d3732e2ce3b45c11d8f9
21mh5a4907 <yadavillivinay50@gmail.com>
2025-02-07 12:16:43 +0530

    Initial Commit

---
+++ pencilsketch/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  pencilsketch
+//
+//  Created by SS-MAC-005 on 07/02/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ pencilsketchTests/pencilsketchTests.swift
@@ -0,0 +1,17 @@
+//
+//  pencilsketchTests.swift
+//  pencilsketchTests
+//
+//  Created by SS-MAC-005 on 07/02/25.
+//
+
+import Testing
+@testable import pencilsketch
+
+struct pencilsketchTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
